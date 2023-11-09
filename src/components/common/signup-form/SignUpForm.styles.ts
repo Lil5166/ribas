0f4e387e90c25a1199212400e9e8d36dbd505970
Yermak Diana <d.yermak.ia23@kpi.ua>
2023-11-09 16:08:11 +0200

    Added styles and validation to Registration

---
+++ src/components/common/signup-form/SignUpForm.styles.ts
@@ -0,0 +1,42 @@
+import { SxProps } from "@mui/system";
+
+export const container: SxProps = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+};
+
+export const h4: SxProps = {
+    marginTop: 4,
+    marginBottom: 4,
+};
+
+export const input: SxProps = {
+    marginTop: 2,
+    marginBottom: 2,
+    width: "100%",
+};
+
+export const inputDate: SxProps = {
+    marginTop: 2,
+    marginBottom: 2,
+    width: "100%",
+};
+export const tableContainer: SxProps = {
+    marginTop: 4,
+    width: "80%",
+};
+
+export const table: SxProps = {
+    minWidth: 650,
+};
+
+export const button: SxProps = {
+    marginTop:2,
+    width: '20%',
+};
+export const buttonSend: SxProps = {
+    display:'flex',
+    justifyContent:'center',
+    width: '20%'
+};
--- src/components/common/signup-form/SignUpForm.tsx
+++ src/components/common/signup-form/SignUpForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
@@ -14,6 +14,7 @@ import {InputLabel, Radio, SelectChangeEvent} from "@mui/material";
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import * as styles from './SignUpForm.styles';
 
 
 interface FormData {
@@ -47,6 +48,16 @@ const SignUpForm: React.FC = () => {
 
     const [formData, setFormData] = useState<FormData>(initialFormData);
     const [tableData, setTableData] = useState<TableData[]>([]);
+    const [emailError, setEmailError] = useState<string | null>(null);
+    const [passwordError, setPasswordError] = useState<string | null>(null);
+    const [surnameError, setSurnameError] = useState<string | null>(null);
+    const [nameError, setNameError] = useState<string | null>(null);
+    const [patronymicError, setPatronymicError] = useState<string | null>(null);
+
+
+    const maxDate = new Date();
+    maxDate.setDate(maxDate.getDate() - 1);
+
 
     const handleGenderChange = (event: SelectChangeEvent<string>) => {
         setFormData({
@@ -55,16 +66,47 @@ const SignUpForm: React.FC = () => {
         });
     };
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
+        const patternEmail = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/;
+        const patternPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W\_]).{8,}$/;
+        const patternSurname = /^([А-ЯІЇЄҐ'’][а-яіїєґ'’]*)(\s[А-ЯІЇЄҐ'’][а-яіїєґ'’]*|-?[А-ЯІЇЄҐ'’][а-яіїєґ'’]*)?$/;
+        const patternName = /^([А-ЯІЇЄҐ'’][а-яіїєґ'’]*)([А-ЯІЇЄҐ'’][а-яіїєґ'’]*|-?[А-ЯІЇЄҐ'’][а-яіїєґ'’]*)?$/;
+        const patternPatronymic = /^([А-ЯІЇЄҐ'’][а-яіїєґ'’]*)(\s[А-ЯІЇЄҐ'’а-яіїєґ'’]*|-?[А-ЯІЇЄҐ'’а-яіїєґ'’]*[А-ЯІЇЄҐ'’][а-яіїєґ'’]*)?$/;
+        switch (name as keyof FormData) {
+            case "email":
+                setEmailError(patternEmail.test(value) ? null : 'Некоректний формат електронної пошти');
+                break;
+            case "password":
+                setPasswordError(patternPassword.test(value) ? null : 'Пароль повинен містити мінімум 1 маленьку літеру, 1 велику літеру, 1 цифру, 1 спеціальний символ і бути не менше 8 символів');
+                break;
+            case 'surname':
+                setSurnameError(patternSurname.test(value) ? null : 'Прізвище повинно мати велику першу літеру і не містити цифр.');
+                break;
+            case 'name':
+                setNameError(patternName.test(value) ? null : 'Ім\'я повинно мати велику першу літеру і не містити цифр.');
+                break;
+            case 'patronymic':
+                setPatronymicError(patternPatronymic.test(value) ? null : 'По-батькові повинно мати велику першу літеру і не містити цифр.');
+                break;
+            default:
+                break;
+        }
+        if (name === 'birthdate') {
+            const selectedDate = new Date(value);
+            if (selectedDate > maxDate) {
+                return;
+            }
+        }
         setFormData({
             ...formData,
             [name]: value,
         });
+
     };
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setTableData([...tableData, { ...formData, selected: false }]);
+        setTableData([...tableData, {...formData, selected: false}]);
         setFormData(initialFormData);
     };
 
@@ -75,65 +117,116 @@ const SignUpForm: React.FC = () => {
     const handleDuplicateSelected = () => {
         const duplicatedData = tableData
             .filter((item) => item.selected)
-            .map((item) => ({ ...item, selected: false }));
+            .map((item) => ({...item, selected: false}));
         setTableData([...tableData, ...duplicatedData]);
     };
+    const handleGroupChange = (event: SelectChangeEvent<string>) => {
+        setFormData({
+            ...formData,
+            group: event.target.value,
+        });
+    };
+
+    const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.replace(/\D/g, '');
+        const maskedValue = value.replace(/^(\d{2})(\d{3})(\d{2})(\d{2})$/, '+38 (0$1) $2-$3-$4');
+        setFormData({
+            ...formData,
+            phone: maskedValue,
+        });
+    };
+
+
+    const formatDate = (dateString: string): string => {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        let month: string | number = date.getMonth() + 1;
+        let day: string | number = date.getDate();
+
+        month = month < 10 ? `0${month}` : month;
+        day = day < 10 ? `0${day}` : day;
+
+        return `${year}-${month}-${day}`;
+    };
+
 
     return (
-        <Container>
-            <Typography variant="h4">Sign up</Typography>
+        <Container sx={styles.container}>
+            <Typography sx={styles.h4} variant="h4">Sign up</Typography>
             <form onSubmit={handleSubmit}>
                 <TextField
+                    sx={styles.input}
                     type={"email"}
                     label="Email"
                     name="email"
                     value={formData.email}
                     onChange={handleInputChange}
+                    error={!!emailError}
+                    helperText={emailError}
                     required
                 />
                 <TextField
+                    sx={styles.input}
                     type={"password"}
-                    label="Passwod"
+                    label="Password"
                     name="password"
                     value={formData.password}
                     onChange={handleInputChange}
+                    error={!!passwordError}
+                    helperText={passwordError}
                     required
                 />
                 <TextField
-                    label="Name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleInputChange}
-                    required
-                />
-                <TextField
+                    sx={styles.input}
                     label="Surname"
                     name="surname"
                     value={formData.surname}
                     onChange={handleInputChange}
+                    error={!!surnameError}
+                    helperText={surnameError}
                     required
                 />
+
                 <TextField
+                    sx={styles.input}
+                    label="Name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    error={!!nameError}
+                    helperText={nameError}
+                    required
+                />
+
+                <TextField
+                    sx={styles.input}
                     label="Patronymic"
                     name="patronymic"
                     value={formData.patronymic}
                     onChange={handleInputChange}
+                    error={!!patronymicError}
+                    helperText={patronymicError}
                     required
                 />
+
                 <TextField
+                    sx={styles.inputDate}
                     type={"date"}
                     label="Birthdate"
                     name="birthdate"
-                    value={formData.birthdate}
+                    value={formatDate(formData.birthdate)}
                     onChange={handleInputChange}
+                    inputProps={{max: maxDate.toISOString().split('T')[0]}}
                     required
                 />
+
                 <TextField
-                    type={"text"}
+                    sx={styles.input}
+                    type="text"
                     label="Phone number"
                     name="phone"
                     value={formData.phone}
-                    onChange={handleInputChange}
+                    onChange={handlePhoneChange}
                     required
                 />
                 <div>
@@ -142,23 +235,24 @@ const SignUpForm: React.FC = () => {
                         name="gender"
                         value="male"
                         checked={formData.gender === 'male'}
-                        onChange={handleInputChange}
+                        onChange={handleGenderChange}
                     />
                     Male
                     <Radio
                         name="gender"
                         value="female"
                         checked={formData.gender === 'female'}
-                        onChange={handleInputChange}
+                        onChange={handleGenderChange}
                     />
                     Female
                 </div>
-                <FormControl>
+                <FormControl sx={styles.input}>
                     <InputLabel htmlFor="group">Group</InputLabel>
                     <Select
                         label="Group"
                         name="group"
                         value={formData.group}
+                        onChange={handleGroupChange}
                         required
                     >
                         <MenuItem value="IA-21">IA-21</MenuItem>
@@ -167,12 +261,25 @@ const SignUpForm: React.FC = () => {
                         <MenuItem value="IA-24">IA-24</MenuItem>
                     </Select>
                 </FormControl>
-                <Button type="submit" variant="contained" color="primary">
+                <Button
+                    sx={styles.buttonSend}
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    disabled={
+                        !!emailError ||
+                        !!passwordError ||
+                        !!surnameError ||
+                        !!nameError ||
+                        !!patronymicError
+                    }
+                >
                     Send
                 </Button>
+
             </form>
-            <TableContainer>
-                <Table>
+            <TableContainer sx={styles.tableContainer}>
+                <Table sx={styles.table}>
                     <TableHead>
                         <TableRow>
                             <TableCell>Select</TableCell>
@@ -211,10 +318,10 @@ const SignUpForm: React.FC = () => {
                     </TableBody>
                 </Table>
             </TableContainer>
-            <Button variant="contained" color="secondary" onClick={handleDeleteSelected}>
+            <Button sx={styles.button} variant="contained" color="secondary" onClick={handleDeleteSelected}>
                 Delete Selected
             </Button>
-            <Button variant="contained" color="primary" onClick={handleDuplicateSelected}>
+            <Button sx={styles.button} variant="contained" color="primary" onClick={handleDuplicateSelected}>
                 Duplicate Selected
             </Button>
         </Container>
