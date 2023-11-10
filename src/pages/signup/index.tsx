83c745203352dc25dcf00a1bfaffc35d55c3bc8f
sasha <o.semashko.ia23@kpi.ua>
2023-11-10 10:27:10 +0200

    add title to registration page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SignUpForm from "@/components/common/signup-form/SignUpForm";
 
 const SignUp = () => {
+    document.title = 'Сторінка реєстрації'
     return (
         <div>
             <SignUpForm />
